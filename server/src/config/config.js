02608c9d49a3d58e764ac46a251a8cd78c23af89
Robert Trainor <trainordevelopments@outlook.com>
2023-01-05 19:15:37 -0600

    SQLite3

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'titanticketp',
+    user: process.env.DB_USER || 'titanticketp',
+    pass: process.env.DB_PASS || 'titanticketp',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './titanticketp.sqlite'
+    }
+  }
+}
