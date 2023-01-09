0799d141811c0339b35eda07e2051742deac5571
Robert Trainor <trainordevelopments@outlook.com>
2023-01-08 21:57:00 -0600

    Added conditional nav-drawer items

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,25 +1,38 @@
 <template>
   <div data-app>
-    <v-app-bar app color="deep-purple accent-4" dark dense>
+    <v-app-bar app color="gray accent-4" dark dense>
       <v-app-bar-nav-icon @click="drawer = true"> </v-app-bar-nav-icon>
       <v-toolbar-title>TitanTicketP</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <register-modal v-if="!$store.state.isUserLoggedIn" />
-      <login-modal v-if="!$store.state.isUserLoggedIn" />
-      <v-btn to="logout" v-if="$store.state.isUserLoggedIn">Logout</v-btn>
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" absolute temporary>
+    <v-navigation-drawer v-model="drawer" absolute temporary bottom>
       <v-list nav dense>
         <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
-          <v-list-item v-for="item in navItems" :key="item.title" link :to="item.to">
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
+          <div v-for="(item, index) in navItems" :key="item.title">
+            <div v-if="item.items" :prepend-icon="item.action" no-action>
+              <br v-if="index !== 0">
+              <v-list-item-title v-text="item.title" no-action text style="text-align:left;"></v-list-item-title>
 
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
+              <div v-for="subItem in item.items" :key="subItem.title">
+                <v-list-item :to="subItem.to" :key="subItem.title" v-if="subItem.stateCheck ? ($store.state[subItem.stateCheck] == subItem.stateStatus) : true">
+                    <v-list-item-icon>
+                      <v-icon v-text="subItem.action"></v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="subItem.title"></v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+              </div>
+            </div>
+
+            <v-list-item v-else :to="item.to" link>
+              <v-list-item-icon>
+                <v-icon v-text="item.action"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </div>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
@@ -35,7 +48,22 @@ export default {
     drawer: false,
     group: null,
     navItems: [
-      { title: 'Home', icon: 'mdi-home', to: '/' }
+      {
+        action: '',
+        title: 'General',
+        items: [
+          { title: 'Home', action: 'mdi-home', to: '/' }
+        ]
+      },
+      {
+        action: '',
+        title: 'Account',
+        items: [
+          { title: 'Login', action: 'mdi-login', to: '/login', stateCheck: `isUserLoggedIn`, stateStatus: false },
+          { title: 'Register', action: 'mdi-account-plus', to: '/register', stateCheck: `isUserLoggedIn`, stateStatus: false },
+          { title: 'Logout', action: 'mdi-logout', to: '/logout', stateCheck: `isUserLoggedIn`, stateStatus: true }
+        ]
+      }
     ]
   }),
   components: {
