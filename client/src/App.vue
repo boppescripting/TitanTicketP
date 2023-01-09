8923e6ccd507bf5d0812ebbcd57189d929efcf2d
Robert Trainor <trainordevelopments@outlook.com>
2023-01-08 19:12:36 -0600

    moved header to own component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,42 +1,17 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <v-app-bar app color="deep-purple accent-4" dark dense>
-      <v-app-bar-nav-icon @click="drawer = true"> </v-app-bar-nav-icon>
-      <v-toolbar-title>TitanTicketP</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon> <v-icon> mdi-heart </v-icon> </v-btn>
-      <v-btn icon> <v-icon> mdi-magnify </v-icon> </v-btn>
-    </v-app-bar>
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav dense>
-        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
-          <v-list-item v-for="item in navItems" :key="item.title" link>
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+    <page-header />
     <router-view />
   </div>
 </template>
 
 <script>
+import PageHeader from '@/components/Header.vue'
 export default {
   name: 'App',
-  data: () => ({
-    drawer: false,
-    group: null,
-    navItems: [
-      { title: 'Home', icon: 'mdi-home' }
-    ]
-  })
+  components: {
+    PageHeader
+  }
 }
 </script>
 
