31efa485af54022cd91ec7a182e113990cbb0ed4
Robert Trainor <trainordevelopments@outlook.com>
2023-01-08 19:13:47 -0600

    Vuex router syncing and store

--- client/src/main.js
+++ client/src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
+import { sync } from 'vuex-router-sync'
+import store from '@/store/store'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
+sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
@@ -21,5 +24,6 @@ new Vue({
     icons: {
       iconfont: 'mdi' // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
     }
-  })
+  }),
+  store
 })
